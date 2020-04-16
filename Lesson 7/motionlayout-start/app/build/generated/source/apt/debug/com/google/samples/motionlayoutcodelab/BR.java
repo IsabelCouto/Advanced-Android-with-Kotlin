aa1a831adc8f85f6c1cd2c780b0d4e92cfdf4c39
Isabel Couto <isabel.couto@creativesystems.eu>
2020-04-16 15:37:14 +0100

    Lesson 8

---
+++ Lesson 7/motionlayout-start/app/build/generated/source/apt/debug/com/google/samples/motionlayoutcodelab/BR.java
@@ -0,0 +1,5 @@
+package com.google.samples.motionlayoutcodelab;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ Lesson 7/motionlayout-start/app/build/generated/source/dataBinding/trigger/debug/com/google/samples/motionlayoutcodelab/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package com.google.samples.motionlayoutcodelab;
+
+import androidx.databinding.BindingBuildInfo;
+
+@BindingBuildInfo
+public class DataBindingInfo {}
